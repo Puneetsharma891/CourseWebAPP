f7579941a98b9cea96a8988ee84a447ce35979e5
adminuser <adminuser@VMWindows>
2021-05-25 17:40:54 +0000

    oauth

--- KeyVaultDemo/Program.cs
+++ KeyVaultDemo/Program.cs
@@ -1,7 +1,10 @@
 ﻿using Azure.Core;
 using Azure.Identity;
+using Azure.Security.KeyVault.Keys;
+using Azure.Security.KeyVault.Keys.Cryptography;
 using Azure.Security.KeyVault.Secrets;
 using System;
+using System.Text;
 
 namespace KeyVaultDemo
 {
@@ -11,8 +14,58 @@ namespace KeyVaultDemo
         {
             FetchSecretUsingAppId();
             Uri _url = new Uri("https://puneetkeyvault.vault.azure.net/");
+            GetSecretUsingManageIdentity(_url);
+            UsingEncryptionKey(_url);
 
-            Console.Read();
+            Console.ReadKey();
+        }
+
+        private static void UsingEncryptionKey(Uri _url)
+        {
+            TokenCredential cred = new DefaultAzureCredential();
+            KeyClient client = new KeyClient(_url, cred);
+            string textToEncrypt = "Sample Text";
+
+            var _key = client.GetKey("EncryptKey");
+
+            // The CryptographyClient class is part of the Azure Key vault package
+            // This is used to perform cryptographic operations with Azure Key Vault keys
+            var crypto_client = new CryptographyClient(_key.Value.Id, cred);
+
+            // We first need to take the bytes of the string that needs to be converted
+
+            byte[] text_to_bytes = Encoding.UTF8.GetBytes(textToEncrypt);
+
+            EncryptResult _result = crypto_client.Encrypt(EncryptionAlgorithm.RsaOaep, text_to_bytes);
+
+            Console.WriteLine("The encrypted text");
+            Console.WriteLine(Convert.ToBase64String(_result.Ciphertext));
+
+            // Now lets decrypt the text
+            // We first need to convert our Base 64 string of the Cipertext to bytes
+
+            byte[] ciper_to_bytes = _result.Ciphertext;
+
+            DecryptResult _text_decrypted = crypto_client.Decrypt(EncryptionAlgorithm.RsaOaep, ciper_to_bytes);
+
+            Console.WriteLine(Encoding.UTF8.GetString(_text_decrypted.Plaintext));
+        }
+
+        private static void GetSecretUsingManageIdentity(Uri _url)
+        {
+            TokenCredential cred = new DefaultAzureCredential();
+
+            SecretClient client = new SecretClient(_url, cred);
+
+            var secret = client.GetSecret("dbpassword");
+
+            Console.Write(secret.Value.Value);
+
+            client.SetSecret("dbpassword", "ChangedData");
+
+            client.SetSecret("Test", "Test");
+
+            client.StartDeleteSecret("Test");
         }
 
         private static void FetchSecretUsingAppId()
---
+++ OAuthDemo/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Azure.Core;
+using Azure.Storage.Blobs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Identity.Web;
+using OAuthDemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OAuthDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private ITokenAcquisition _token;
+        public HomeController(ILogger<HomeController> logger, ITokenAcquisition token)
+        {
+            _logger = logger;
+            _token = token;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            string[] scopes = new string[] { "https://storage.azure.com/user_impersonation" };
+            string token = await _token.GetAccessTokenForUserAsync(scopes);
+            ViewBag.token = token;
+
+            BlobClient client = new BlobClient(new Uri("https://storageclitest2021.blob.core.windows.net/mycontainer2021/Course0.json"
+                ), new TokenAcquisitionTokenCredential(_token));
+
+            MemoryStream stream = new MemoryStream();
+            client.DownloadTo(stream);
+            stream.Position = 0;
+            StreamReader rdr = new StreamReader(stream);
+            string str = rdr.ReadToEnd();
+            ViewBag.Content = str;
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
