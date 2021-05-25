0b5684547802eb5a0dc5fceffc70a2f750734176
Puneet Sharma <puneetsharma89@gmail.com>
2021-05-25 14:00:47 +0530

    keyvault changes

--- BolobStorageDemo/Program.cs
+++ BolobStorageDemo/Program.cs
@@ -1,10 +1,13 @@
-﻿using Azure.Storage.Blobs;
+﻿using Azure.Identity;
+using Azure.Storage.Blobs;
 using Azure.Storage.Blobs.Models;
 using Azure.Storage.Sas;
 using Microsoft.Azure.Storage;
 using Microsoft.Azure.Storage.Auth;
 using Microsoft.Azure.Storage.Blob;
 using System;
+using System.Collections.Generic;
+using BlobProperties = Azure.Storage.Blobs.Models.BlobProperties;
 
 namespace BolobStorageDemo
 {
@@ -16,12 +19,34 @@ namespace BolobStorageDemo
             string _conn = "DefaultEndpointsProtocol=https;AccountName=storageclitest2021;AccountKey=UlOoeJuTkCn7L3tiO0Glap/+zyCQ4kHA5mIl2CEsMWSW9p0OY/W39og+iIq3V9luGHaOXd2lU9ZggG4GuZmp2Q==;EndpointSuffix=core.windows.net";
             //OldBlobClient(cred);
 
-
-
+            BlobClient _blob = AccessBlobUsingAppObject();
+            ReadSetMetaData(_blob);
 
             NewBlobClient(containername, _conn);
 
             Console.Read();
+        }
+
+        private static void ReadSetMetaData(BlobClient _blob)
+        {
+            BlobProperties props = _blob.GetProperties();
+
+            IDictionary<string, string> metadata = props.Metadata;
+            metadata.Add("Test", "Test");
+            _blob.SetMetadata(metadata);
+        }
+
+        private static BlobClient AccessBlobUsingAppObject()
+        {
+            string appId = "a36d46c9-9335-4137-9e2f-d0079fc15ff5";
+            string tenantId = "9a0c6406-e26c-4288-a20c-4df2a8eb78a7";
+            string secretId = "pbg-ZlEbO_.4GxC7.023jthP-oV0U_8o6s";
+            string blobUrl = "https://storageclitest2021.blob.core.windows.net/mycontainer2021/Course0.json";
+
+            BlobClient _blob = new BlobClient(new Uri(blobUrl), new ClientSecretCredential(tenantId, appId, secretId));
+
+            _blob.DownloadTo(@"d:\" + _blob.Name);
+            return _blob;
         }
 
         private static void OldBlobClient()
@@ -62,6 +87,12 @@ namespace BolobStorageDemo
             string blobName = "Course1.json";
             Uri sasURL = GenerateSasURL(containerClient.Name, blobName, containerClient);
             BlobClient _clientBlob = new BlobClient(sasURL);
+            Azure.Storage.Blobs.Models.BlobProperties props = _clientBlob.GetProperties();
+
+            IDictionary<string, string> metadata = props.Metadata;
+            metadata.Add("test", "test");
+            _clientBlob.SetMetadata(metadata);
+
             _clientBlob.DownloadTo(@"E:\sasJSONDemo.json");
         }
 
---
+++ KeyVaultDemo/Program.cs
@@ -0,0 +1,31 @@
+﻿using Azure.Core;
+using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
+using System;
+
+namespace KeyVaultDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FetchSecretUsingAppId();
+            Uri _url = new Uri("https://puneetkeyvault.vault.azure.net/");
+
+            Console.Read();
+        }
+
+        private static void FetchSecretUsingAppId()
+        {
+            string appId = "a36d46c9-9335-4137-9e2f-d0079fc15ff5";
+            string tenantId = "9a0c6406-e26c-4288-a20c-4df2a8eb78a7";
+            string secretId = "pbg-ZlEbO_.4GxC7.023jthP-oV0U_8o6s";
+            string secretName = "dbpassword";
+            Uri _url = new Uri("https://puneetkeyvault.vault.azure.net/");
+            ClientSecretCredential cred = new ClientSecretCredential(tenantId, appId, secretId);
+            SecretClient _client = new SecretClient(_url, cred);
+            var secret = _client.GetSecret(secretName);
+            Console.WriteLine(secret.Value.Value);
+        }
+    }
+}
